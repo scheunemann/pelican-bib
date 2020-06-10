41186620fe454bf69c3f2cda77c55733e96e4e80
Daniel Richter <github@crftartsc.de>
2020-06-10 15:19:56 +0200

    override current signature of pybtex.*.unsrt https://bitbucket.org/pybtex-devs/pybtex/src/master/pybtex/style/formatting/unsrt.py

--- pelican_bib/tagdecorator.py
+++ pelican_bib/tagdecorator.py
@@ -15,85 +15,88 @@ def get_style_class(cls,decorate_html):
 
     class HtmlDecorator(cls):
 
-        def format_names(self, role, as_sentence=True): 
+        def format_names(self, role, as_sentence=True):
             return tag(':bib-names') [ super().format_names(role, as_sentence) ]
 
-        def format_article(self, e): 
-            return tag(':bib-article') [ super().format_article(e) ]
+        def get_article_template(self, e):
+            return tag(':bib-article') [ super().get_article_template(e) ]
 
-        def format_author_or_editor(self, e): 
+        def format_author_or_editor(self, e):
             return tag(':bib-author_or_editor') [ super().format_author_or_editor(e) ]
 
-        def format_editor(self, e, as_sentence=True): 
+        def format_editor(self, e, as_sentence=True):
             return tag(':bib-editor') [ super().format_editor(e, as_sentence) ]
 
-        def format_volume_and_series(self, e, as_sentence=True): 
+        def format_volume_and_series(self, e, as_sentence=True):
             return tag(':bib-volume_and_series') [ super().format_volume_and_series(e, as_sentence) ]
 
-        def format_chapter_and_pages(self, e): 
+        def format_chapter_and_pages(self, e):
             return tag(':bib-chapter_and_pages') [ super().format_chapter_and_pages(e) ]
 
-        def format_edition(self, e): 
+        def format_edition(self, e):
             return tag(':bib-edition') [ super().format_edition(e) ]
 
-        def format_title(self, e, which_field, as_sentence=True): 
+        def format_title(self, e, which_field, as_sentence=True):
             return tag(':bib-title') [ super().format_title(e, which_field, as_sentence) ]
 
-        def format_btitle(self, e, which_field, as_sentence=True): 
+        def format_btitle(self, e, which_field, as_sentence=True):
             return tag(':bib-btitle') [ super().format_btitle(e, which_field, as_sentence) ]
 
-        def format_address_organization_publisher_date(self, e, include_organization=True): 
+        def format_address_organization_publisher_date(self, e, include_organization=True):
             return tag(':bib-address_organization_publisher_date') [ super().format_address_organization_publisher_date(e, include_organization) ]
 
-        def format_book(self, e): 
-            return tag(':bib-book') [ super().format_book(e) ]
+        def get_book_template(self, e):
+            return tag(':bib-book') [ super().get_book_template(e) ]
 
-        def format_booklet(self, e): 
-            return tag(':bib-booklet') [ super().format_booklet(e) ]
+        def get_booklet_template(self, e):
+            return tag(':bib-booklet') [ super().get_booklet_template(e) ]
 
-        def format_inbook(self, e): 
-            return tag(':bib-inbook') [ super().format_inbook(e) ]
+        def get_inbook_template(self, e):
+            return tag(':bib-inbook') [ super().get_inbook_template(e) ]
 
-        def format_incollection(self, e): 
-            return tag(':bib-incollection') [ super().format_incollection(e) ]
+        def get_incollection_template(self, e):
+            return tag(':bib-incollection') [ super().get_incollection_template(e) ]
 
-        def format_inproceedings(self, e): 
-            return tag(':bib-inproceedings') [ super().format_inproceedings(e) ]
+        def get_inproceedings_template(self, e):
+            return tag(':bib-inproceedings') [ super().get_inproceedings_template(e) ]
 
-        def format_manual(self, e): 
-            return tag(':bib-manual') [ super().format_manual(e) ]
+        def get_manual_template(self, e):
+            return tag(':bib-manual') [ super().get_manual_template(e) ]
 
-        def format_mastersthesis(self, e): 
-            return tag(':bib-mastersthesis') [ super().format_mastersthesis(e) ]
+        def get_mastersthesis_template(self, e):
+            return tag(':bib-mastersthesis') [ super().get_mastersthesis_template(e) ]
 
-        def format_misc(self, e): 
-            return tag(':bib-misc') [ super().format_misc(e) ]
+        def get_misc_template(self, e):
+            return tag(':bib-misc') [ super().get_misc_template(e) ]
 
-        def format_phdthesis(self, e): 
-            return tag(':bib-phdthesis') [ super().format_phdthesis(e) ]
+        def get_phdthesis_template(self, e):
+            return tag(':bib-phdthesis') [ super().get_phdthesis_template(e) ]
 
-        def format_proceedings(self, e): 
-            return tag(':bib-proceedings') [ super().format_proceedings(e) ]
+        def get_proceedings_template(self, e):
+            return tag(':bib-proceedings') [ super().get_proceedings_template(e) ]
 
-        def format_techreport(self, e): 
-            return tag(':bib-techreport') [ super().format_techreport(e) ]
+        def get_techreport_template(self, e):
+            return tag(':bib-techreport') [ super().get_techreport_template(e) ]
 
-        def format_unpublished(self, e): 
-            return tag(':bib-unpublished') [ super().format_unpublished(e) ]
+        def get_unpublished_template(self, e):
+            return tag(':bib-unpublished') [ super().get_unpublished_template(e) ]
 
-        def format_web_refs(self, e): 
+        def format_web_refs(self, e):
             return tag(':bib-web_refs') [ super().format_web_refs(e) ]
 
-        def format_url(self, e): 
+        def format_url(self, e):
             return tag(':bib-url') [ super().format_url(e) ]
 
-        def format_pubmed(self, e): 
+        def format_pubmed(self, e):
             return tag(':bib-pubmed') [ super().format_pubmed(e) ]
 
-        def format_doi(self, e): 
+        def format_doi(self, e):
             return tag(':bib-doi') [ super().format_doi(e) ]
 
-        def format_eprint(self, e): 
+        def format_eprint(self, e):
             return tag(':bib-eprint') [ super().format_eprint(e) ]
+
+        def format_isbn(self, e):
+            return tag(':bib-isbn') [ super().format_isbn(e) ]
 
     return HtmlDecorator
